9019c873c8ce2dc153d07b6caa7a0246efbcdd84
Fabian Meyer <meyerfabian1988@gmail.com>
2023-08-31 10:28:16 +0200

    commenting upload needs to be different

--- app/api/upload_tv/route.tsx
+++ app/api/upload_tv/route.tsx
@@ -3,20 +3,20 @@ import { NextResponse as nextResponse } from 'next/server';
 import fs from 'fs';
 const jsonDataString = fs.readFileSync('app/api/upload_tv/tv_data.json').toString();
 
-export async function GET(request: Request) {
+// export async function GET(request: Request) {
   
-  const data = JSON.parse(jsonDataString);
-  try {
+//   const data = JSON.parse(jsonDataString);
+//   try {
 
-    for (const item of data) {
-      await postgresSql`
-        INSERT INTO stock_symbols (exchange, symbol)
-        VALUES (${item.exchange}, ${item.symbol})
-      `;
-    }
+//     for (const item of data) {
+//       await postgresSql`
+//         INSERT INTO stock_symbols (exchange, symbol)
+//         VALUES (${item.exchange}, ${item.symbol})
+//       `;
+//     }
 
-    return nextResponse.json({ data }, { status: 200 });
-  } catch (error) {
-    return nextResponse.json({ error }, { status: 500 });
-  }
-}
+//     return nextResponse.json({ data }, { status: 200 });
+//   } catch (error) {
+//     return nextResponse.json({ error }, { status: 500 });
+//   }
+// }
