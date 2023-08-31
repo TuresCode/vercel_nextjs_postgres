b99e3805ae617dbc8c283592eab1031503b822eb
Fabian Meyer <meyerfabian1988@gmail.com>
2023-08-31 14:15:34 +0200

    using tsvector

--- app/api/get-stock/route.tsx
+++ app/api/get-stock/route.tsx
@@ -8,7 +8,7 @@ export async function GET(request: Request) {
   try {
     // Fetch data from the SQL database using the provided symbol
     const result = await sql`
-                SELECT * FROM stock_symbols WHERE symbol LIKE CONCAT('%', ${symbol}, '%');
+                SELECT * FROM stock_symbols WHERE to_tsvector('english', symbol) @@ to_tsquery('english', ${symbol});
             `;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
