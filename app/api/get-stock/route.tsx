5f0ee9ebcd581fab4848e0b1c2690d7128cfd853
Fabian Meyer <meyerfabian1988@gmail.com>
2023-09-04 11:13:23 +0200

    changing database

--- app/api/get-stock/route.tsx
+++ app/api/get-stock/route.tsx
@@ -4,13 +4,19 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const symbol = searchParams.get("symbol");
+  const pagesize = 20;
 
   try {
-    // Fetch data from the SQL database using the provided symbol
-    const result = await sql`
-                SELECT * FROM stock_symbols WHERE to_tsvector('english', symbol) @@ to_tsquery('english', ${symbol});
+    // Fetch data from the SQL database using the provided symbol using tsvectors and tsquery
+    // const result = await sql`
+    //             SELECT * FROM stock_symbols WHERE to_tsvector('english', symbol) @@ to_tsquery('english', ${symbol});
+    //         `;
+    // return NextResponse.json({ result }, { status: 200 });
+    // new way to query from another table
+      const result = await sql`
+                SELECT * FROM tv WHERE exchange LIKE ${symbol} OR symbol LIKE ${symbol} OR desc LIKE ${symbol} LIMIT ${pagesize}"
             `;
-    return NextResponse.json({ result }, { status: 200 });
+      return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
