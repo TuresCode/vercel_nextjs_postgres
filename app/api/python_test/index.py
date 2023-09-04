9797405be95b93d510b0c447602da8f8aaa0e4b9
Fabian Meyer <meyerfabian1988@gmail.com>
2023-09-04 14:45:00 +0200

    no math

--- app/api/python_test/index.py
+++ app/api/python_test/index.py
@@ -3,11 +3,8 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        a=1000
-        b=2000
-        c = a + b
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(f'Hello, world! {c}'.encode('utf-8'))
+        self.wfile.write(f'Hello, world!'.encode('utf-8'))
         return
