eef3fdce79a93d1e0e8720f6939f466abb4a232c
Fabian Meyer <meyerfabian1988@gmail.com>
2023-09-04 11:58:45 +0200

    without limit

--- app/api/get-stock/route.tsx
+++ app/api/get-stock/route.tsx
@@ -4,7 +4,6 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const symbol = searchParams.get("symbol");
-  const pagesize = 20;
 
   try {
     // Fetch data from the SQL database using the provided symbol using tsvectors and tsquery
@@ -14,7 +13,7 @@ export async function GET(request: Request) {
     // return NextResponse.json({ result }, { status: 200 });
     // new way to query from another table
       const result = await sql`
-                SELECT * FROM tv WHERE exchange LIKE ${symbol} OR symbol LIKE ${symbol} OR description LIKE ${symbol} LIMIT ${pagesize}"
+                SELECT * FROM tv WHERE exchange LIKE ${symbol} OR symbol LIKE ${symbol} OR description LIKE ${symbol}"
             `;
       return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
